2fccb1c71dfbc5247ffc9734c253f63fb9d6db62
elmiller17 <echolsmiller17@comcast.net>
2017-08-16 15:59:06 -0400

    second commit

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -15,9 +15,45 @@ import Bars from 'react-icons/lib/fa/bars';
 
 
 
+
 export default class Navbar extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      menuOpen:false
+    }
+  }
+
+  handleMenu =() => {
+    if(this.state.menuOpen === true)
+    {
+      this.setState({
+        menuOpen:false
+      })
+    }
+    else if(this.state.menuOpen === false)
+    {
+      this.setState({
+        menuOpen:true
+      })
+    }
+  }
+
+  renderMenu() {
+    if(this.state.menuOpen === true) {
+      return (
+        <nav className="navMobile">
+          <Link to="/" className="navButton">Home</Link>
+          <Link to="contact" className="navButton">Contact</Link>
+          <a href="https://github.com/Technpoathic" className="navButton">Github</a>
+        </nav>
+      )
+    }
+  }
+
+
   render() {
-    return (
+    return(
       <div>
         <div className= "navBar">
           <div className="siteName">Site Name</div>
@@ -27,6 +63,21 @@ export default class Navbar extends React.PureComponent {
             <Link to ="/contact" className="navButton">Contact</Link>
             <a href="https://github.com/Technpoathic" className="navButton">Github</a>
           </nav>
+          <nav className="nav">
+            <Link to="/" className="navButton">Home</Link>
+            <Link to="/contact" className="navButton">Contact</Link>
+            <a href="https://github.com/Technpoathic" className="navButton">Github</a>
+          </nav>
+
+          <Bars className="menuIcon" onClick={this.handleMenu}/>
+
+          <nav className="nav">
+            <Link to="/" className="navButton">Home</Link>
+            <Link to="/contact" className="navButton">Contact</Link>
+            <a href="https://github.com/Technpoathic" className="navButton">Github</a>
+          </nav>
+
+          <Bars className="menuIcon" onClick={this.handleMenu}/>
 
 
         </div>
